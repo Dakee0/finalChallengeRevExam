0b1770621c453f73876142f2a7f3e3df0997d460
Loïc Monnerat <loic.monnerat@heig-vd.ch>
2024-02-01 11:58:42 +0100

    Modif du code selon exactement contenu du prof

--- src/modules/Color.js
+++ src/modules/Color.js
@@ -1,34 +1,48 @@
 import * as convert from "color-convert";
 
-export class Color{
-    #hsl;
-    #hex;
-    #element;
+export class Color {
+  // Crée un champ privé #hsl
+  #hsl;
+  // Crée un champ privé #hex
+  #hex;
+  // Crée un champ privé #element
+  #element;
 
-    constructor(hsl){
-        this.#hsl = hsl;
-        this.#hex = `#${convert.hsl.hex(hsl)}`;
-        this.#element = this.#generateElement();
-    }
+  constructor(hsl) {
+    this.#hsl = hsl;
 
-    #generateElement(){
-        //Création élément <div>
-        const colorElement = document.createElement("div");
-        colorElement.classList.add("color");
-        colorElement.dataset.color = this.#hex;
-        colorElement.style.backgroundColor = this.#hex;
+    // Converti la valeur hsl en hexadécimal
+    this.#hex = `#${convert.hsl.hex(hsl)}`;
 
-        //Création élément <p>
-        const textElement = document.createElement("p");
-        textElement.textContent = this.#hex;
-        textElement.style.color = this.#hsl[2] < 60 ? "#ffffff" : "#000000";
+    // Crée l'élément
+    this.#element = this.#generateElement();
+  }
 
-        colorElement.appendChild(textElement);
+  #generateElement() {
+    // Crée un élément <div>
+    const colorElement = document.createElement("div");
+    // Lui ajoute une class "color"
+    colorElement.classList.add("color");
+    // Ajoute l'attribut de donnée "data-color"
+    colorElement.dataset.color = this.#hex;
+    // Change la couleur de fond de l'élément
+    colorElement.style.backgroundColor = this.#hex;
 
-        return colorElement;
-    }
+    //Crée un élément <p>
+    const textElement = document.createElement("p");
+    // Lui ajoute comme texte la valeur hexadécimale
+    textElement.textContent = this.#hex;
+    // Change la couleur du texte selon la luminosité de la couleur de fond
+    textElement.style.color = this.#hsl[2] < 60 ? "#ffffff" : "#000000";
+    // Ajoute l'élément <p> comme enfant du <div>
+    colorElement.appendChild(textElement);
 
-    display(parentElement){
-        parentElement.appendChild(this.#element);
-    }
+    // Retourne le <div>
+    return colorElement;
+  }
+
+  display(parentElement) {
+    // Ajoute this.#element comme enfant d'un élément parent passé en argument.
+    parentElement.appendChild(this.#element);
+  }
 }
--- src/modules/ColorPal.js
+++ src/modules/ColorPal.js
@@ -1,27 +1,30 @@
 import * as convert from "color-convert";
 
-export const generatePalette = (hex) =>{
-    //création d'un tableau vide
-    const colors = [];
+export const generatePalette = (hex) => {
+  // crée un tableau vide.
+  const colors = [];
 
-    //Converti le hex donné en tab hsl, il ne prend que les 2 premières valeures
-    const [h, s] = convert.hex.hsl(hex);
+  // converti le hex d'entrée en tableau hsl. Ne récupérer que les
+  // deux premières valeurs.
+  const [h, s] = convert.hex.hsl(hex);
 
-    //Itère entre 0 et 100 par intervalle de 10
-    for(let i = 0; i <= 100; i +=10){
-        //chaque itération, on pousse un nouveau tableau dans color
-        // la teinte et saturation sont fixe, la luminosité évolue
-        colors.push([h, s, i])
-    }
+  // Itère entre 0 et 100 par intervalle de 10
+  for (let i = 0; i <= 100; i += 10) {
+    // À chaque itération, pousse un nouveau tableau dans colors contenant
+    // la teinte (fixe), la saturation (fixe) et la luminosité déterminée par
+    // l'index de la boucle.
+    colors.push([h, s, i]);
+  }
 
-    return colors;
-}
-
-export const isHexColor = (hex) => /^#[0-9A-F]{6}$/i.test(hex);
+  // Retourne le tableau.
+  return colors;
+};
 
 export const hexToCSSHSL = (hex) => {
-	// tranforme le hex d'entrée en HSL.
+  // tranforme le hex d'entrée en HSL.
   const hsl = convert.hex.hsl(hex);
-	// Retourne une chaîne de caractère au format css.
+  // Retourne une chaîne de caractère au format css.
   return `${hsl[0]}deg ${hsl[1]}% ${hsl[2]}%`;
 };
+
+export const isHexColor = (hex) => /^#[0-9A-F]{6}$/i.test(hex);
